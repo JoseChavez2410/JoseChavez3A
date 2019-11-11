8ba719b2641f26f4f48760244b9549ddd1f7870e
JoseChavez2410 <2410bautista@gmail.com>
2019-11-10 23:14:42 -0500

    Programa

---
+++ Deber3VentaGasolina/Deber3VentaGasolina/Clases/Clientes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deber3VentaGasolina.Clases
+{
+    class Clientes
+    {
+        private string apellidos;
+
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        private string nombres;
+
+        public string Nombres
+        {
+            get { return nombres; }
+            set { nombres = value; }
+        }
+
+        private int cedula;
+
+        public int Cedula
+        {
+            get { return cedula; }
+            set { cedula = value; }
+        }
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+    }
+}
---
+++ Deber3VentaGasolina/Deber3VentaGasolina/Clases/Gasolina.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deber3VentaGasolina.Clases
+{
+    class Gasolina
+    {
+        private string tipo;
+
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private int cantidad;
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        private float precio;
+
+        public float Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+    }
+}
