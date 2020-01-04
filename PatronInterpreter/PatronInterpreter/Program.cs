5ef88f9d7f8c17c02b8b4b8b778d5734bf2e3750
JoseChavez2410 <2410bautista@gmail.com>
2020-01-04 14:24:08 -0500

    CorregidoFinalizado

--- PatronInterpreter/PatronInterpreter/Program.cs
+++ PatronInterpreter/PatronInterpreter/Program.cs
@@ -11,6 +11,12 @@ namespace PatronInterprete
     {
         static void Main(string[] args)
         {
+            //Este programa evaluara un numero que está en notacion Romana, y lo interpretará para 
+            //arrojar un resultado en notacion Decimal, para la interpretacion se apoyará en las clases 
+            //que forman parte del patron Interpreter realizando operaciones para conseguir el resultado.
+
+            //Para verificar el correcto funcionamiento puede cambiar el valor de 'expresionEvaluar', por un numero Romano valido.
+
             string expresionEvaluar = "MMMCMCV";
             //se crea el objeto contexto y le pasamos la expresion a evaluar
             Contexto contexto = new Contexto(expresionEvaluar);
